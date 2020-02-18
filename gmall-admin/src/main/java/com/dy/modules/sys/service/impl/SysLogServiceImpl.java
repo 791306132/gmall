dbcef87f8f21c206ed9c0bcdeeb1b0fc24aa9c94
Jason <791306132@qq.com>
2020-02-18 15:48:08 +0800

    first commit

---
+++ gmall-admin/src/main/java/com/dy/modules/sys/service/impl/SysLogServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2016-2019 谷粒开源 All rights reserved.
+ *
+ * https://www.guli.cloud
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.dy.modules.sys.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dy.common.utils.PageUtils;
+import com.dy.common.utils.Query;
+import com.dy.modules.sys.dao.SysLogDao;
+import com.dy.modules.sys.entity.SysLogEntity;
+import com.dy.modules.sys.service.SysLogService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+
+@Service("sysLogService")
+public class SysLogServiceImpl extends ServiceImpl<SysLogDao, SysLogEntity> implements SysLogService {
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String)params.get("key");
+
+        IPage<SysLogEntity> page = this.page(
+            new Query<SysLogEntity>().getPage(params),
+            new QueryWrapper<SysLogEntity>().like(StringUtils.isNotBlank(key),"username", key)
+        );
+
+        return new PageUtils(page);
+    }
+}
