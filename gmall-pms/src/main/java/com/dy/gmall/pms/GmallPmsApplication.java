473355f15106c7bb4f20807b704d2860974aca1a
Jason <791306132@qq.com>
2020-02-19 11:59:57 +0800

    first commit

---
+++ gmall-pms/src/main/java/com/dy/gmall/pms/GmallPmsApplication.java
@@ -0,0 +1,19 @@
+package com.dy.gmall.pms;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.dy.gmall.pms.dao ")
+@EnableSwagger2
+public class GmallPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallPmsApplication.class, args);
+	}
+
+}
---
+++ gmall-ums/src/main/java/com/dy/gmall/ums/GmallUmsApplication.java
@@ -0,0 +1,19 @@
+package com.dy.gmall.ums;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@MapperScan("com.dy.gmall.ums.dao")
+@EnableDiscoveryClient
+public class GmallUmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUmsApplication.class, args);
+    }
+
+}
